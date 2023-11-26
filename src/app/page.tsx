b1de476e3d586fe1d23c4cf7edb527cb8a7b0fe2
Cerchie <luciacerchie@gmail.com>
2023-11-26 12:59:43 -0800

    add values for spring func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,48 +3,73 @@
 import React, { useState } from 'react';
 
 const MyFormPage: React.FC = () => {
-  const [inputValue1, setInputValue1] = useState<string>('');
-  const [inputValue2, setInputValue2] = useState<string>('');
-
+  const [mass, setMass] = useState<string>('');
+  const [stiffness, setStiffness] = useState<string>('');
+  const [damping, setDamping] = useState<string>('');
+  const [velocity, setVelocity] = useState<string>('');
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // Pass the input values to your function
-    handleFormSubmit(inputValue1, inputValue2);
+    handleFormSubmit(mass, stiffness, damping, velocity);
   };
 
-  const handleInputChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue1(event.target.value);
+  const handleInputChangeMass = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMass(event.target.value);
   };
 
-  const handleInputChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue2(event.target.value);
+  const handleInputChangeStiffness = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStiffness(event.target.value);
+  };
+  const handleInputChangeDamping = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDamping(event.target.value);
   };
 
-  const handleFormSubmit = (value1: string, value2: string) => {
+  const handleInputChangeVelocity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setVelocity(event.target.value);
+  };
+  const handleFormSubmit = (value1: string, value2: string, value3: string, value4: string) => {
     // Your function that uses the form input values
     // For demonstration purposes, we'll just log them to the console
     console.log('Form input value 1:', value1);
     console.log('Form input value 2:', value2);
+    console.log('Form input value 1:', value3);
+    console.log('Form input value 2:', value4);
   };
 
   return (
     <div>
       <h1>Form Page</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="inputField1">Enter value 1:</label>
+        <label htmlFor="massField">Enter mass:</label>
         <input
           type="text"
-          id="inputField1"
-          value={inputValue1}
-          onChange={handleInputChange1}
+          id="massField"
+          value={mass}
+          onChange={handleInputChangeMass}
         />
 
-        <label htmlFor="inputField2">Enter value 2:</label>
+<label htmlFor="stiffnessField">Enter stiffness:</label>
         <input
           type="text"
-          id="inputField2"
-          value={inputValue2}
-          onChange={handleInputChange2}
+          id="stiffnessField"
+          value={stiffness}
+          onChange={handleInputChangeStiffness}
+        />
+
+        <label htmlFor="velocityField">Enter velocity:</label>
+        <input
+          type="text"
+          id="velocityField"
+          value={velocity}
+          onChange={handleInputChangeVelocity}
+        />
+
+<label htmlFor="dampingField">Enter damping:</label>
+        <input
+          type="text"
+          id="dampingField"
+          value={damping}
+          onChange={handleInputChangeDamping}
         />
 
         <button type="submit">Submit</button>
